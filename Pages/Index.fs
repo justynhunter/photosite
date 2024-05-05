13732d2f5a805e7600cd3cef38e6380f652702a2
Justyn Hunter <justynhunter@gmail.com>
2024-05-05 15:13:50 -0500

    set image width so its reactive

--- Pages/Index.fs
+++ Pages/Index.fs
@@ -7,7 +7,5 @@ let body =
         Html.img [
             prop.src "https://live.staticflickr.com/65535/53609244371_184f1a88c1_b.jpg"
             prop.alt "rotting house"
-            prop.width 1024
-            prop.height 768
         ]
     ] |> Layout.main "Justyn Hunter"
