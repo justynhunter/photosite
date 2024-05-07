226cde07d195b0e39cd8bd66681b3ddeb9bb2925
Justyn Hunter <justynhunter@gmail.com>
2024-05-07 11:06:25 -0500

    stub out image switching

--- Pages/Index.fs
+++ Pages/Index.fs
@@ -2,9 +2,54 @@ module IndexPage
 
 open Giraffe.ViewEngine
 
-let body =
-    img [ 
-        _src "https://live.staticflickr.com/65535/53609244371_184f1a88c1_b.jpg"
-        _alt "rotting house"
-    ] |> Layout.main "justyn hunter"
+type photo = {
+    id: int
+    nextId: int
+    prevId: int
+    src: string
+    alt: string
+}
 
+let photos = [
+    { 
+        id = 1
+        nextId = 2
+        prevId = 3
+        src = "https://live.staticflickr.com/65535/53609244371_184f1a88c1_b.jpg"
+        alt = "rotting house"
+    }
+    {
+        id = 2
+        nextId = 3
+        prevId = 1
+        src = "https://live.staticflickr.com/65535/53684852131_5d1a0c2a54_b.jpg"
+        alt = "chimney"
+    }
+    { 
+        id = 3
+        nextId = 1
+        prevId = 2
+        src = "https://live.staticflickr.com/65535/53684851241_bac2ceb36b_b.jpg"
+        alt = "rotting house"
+    }
+]
+
+let private getPhoto (id : int) =
+    List.find (fun p -> p.id = id) photos
+
+let getPhotoElement (photo : photo) =
+    div [ _id "image_container" ] [
+        button [ _id "btn_prev"; _value (string photo.prevId) ] []
+        img [
+            _id "photo"
+            _src photo.src
+            _alt photo.alt
+        ]
+        button [ _id "btn_next"; _value (string photo.nextId)] []
+    ]
+
+let body =
+    1
+    |> getPhoto 
+    |> getPhotoElement
+    |> Layout.main "justyn hunter"
