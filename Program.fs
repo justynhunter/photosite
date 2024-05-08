7a3f146fd4d796ed882e07e8e454d20c3722acb4
Justyn Hunter <justynhunter@gmail.com>
2024-05-08 10:07:47 -0500

    endpoint routing

--- Program.fs
+++ Program.fs
@@ -1,8 +1,10 @@
 ﻿open Giraffe
+open Giraffe.EndpointRouting
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
+open Microsoft.AspNetCore.Http
 
 let renderComponent nodes : HttpHandler =
         noResponseCaching
@@ -10,20 +12,43 @@ let renderComponent nodes : HttpHandler =
             return! ctx.WriteHtmlStringAsync (ViewEngine.RenderView.AsString.htmlNodes nodes)
         }
 
-let webApp =
-    choose [
-        route "/" >=> htmlString (IndexPage.page 1)
-        route "/about" >=> htmlString AboutPage.page
-        route "/contact" >=> htmlString ContactPage.page
-        route "/api/contact-submit" >=> renderComponent ContactPage.submit
-        routef "/api/get-photo/%d" (Data.getPhoto >> ImageCarousel.nodes >> renderComponent)
-        routef "/%d" (fun (id : int64) -> IndexPage.page id |> htmlString) 
+[<CLIMutable>]
+type contactFormSubmission = {
+    message : string
+}
+
+let contactFormHandler : HttpHandler =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let! submission = ctx.BindFormAsync<contactFormSubmission>();
+
+            // TODO: save to db
+
+            return! renderComponent ContactPage.submit next ctx
+        }
+
+let endpoints =
+    [
+        GET [
+            route "/" (1L |> IndexPage.page |> htmlString)
+            route "/about" (htmlString AboutPage.page)
+            route "/contact" (htmlString ContactPage.page) 
+            routef "/%d" (fun (id:int64) -> IndexPage.page id |> htmlString)
+        ]
+        POST [
+            subRoute "/api" [
+                route "/contact-submit" contactFormHandler
+                routef "/get-photo/%d" (Data.getPhoto >> ImageCarousel.nodes >> renderComponent)
+            ]
+        ]
     ]
 
 let configureApp (app : IApplicationBuilder) =
     app
         .UseStaticFiles() 
-        .UseGiraffe(webApp)
+        .UseRouting()
+        .UseEndpoints(fun e -> e.MapGiraffeEndpoints(endpoints))
+    |> ignore
 
 let configureServices (services : IServiceCollection) =
     services.AddGiraffe() |> ignore
