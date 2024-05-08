d629364505eaccdb9d3793aa13980f2652fe73bf
Justyn Hunter <justynhunter@gmail.com>
2024-05-07 22:39:07 -0500

    return something for a form submission

--- Pages/Contact.fs
+++ Pages/Contact.fs
@@ -1,19 +1,34 @@
 module ContactPage
 
 open Giraffe.ViewEngine
+open Giraffe.ViewEngine.Htmx
 
 let page =
     [
-        form [] [
-            label [ _for "#text" ] [
-                str "enter your message below"
-            ]
-            textarea [ _id "text" ] []
-            input [
-                _type "submit"
-                _value "send"
+        div [ _id "form_container" ] [
+            form [
+                _hxPost "api/contact-submit"
+                _hxTarget "#form_container"
+                _hxSwap "innerHTML"
+            ] [
+                label [ _for "#text" ] [
+                    str "enter your message below"
+                ]
+                textarea [ _id "text" ] []
+                input [
+                    _type "submit"
+                    _value "send"
+                ]
             ]
         ]
     ]
     |> Layout.main "contact"
 
+let submit =
+    [
+        div [] [
+            p [] [
+                str "Thank you for your message!"
+            ]
+        ]
+    ]
