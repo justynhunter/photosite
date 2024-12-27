259de6dd54a61913dcc31ea307ee16bec9710a16
Justyn Hunter <cjhunter@me.com>
2024-12-27 10:17:40 -0600

    configure aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src/"),
+            "@pages": path.resolve(__dirname, "./src/pages/"),
+            "@components": path.resolve(__dirname, "./src/components/"),
+        },
+    },
+    plugins: [react()],
+});
