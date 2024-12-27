25762dc3fb711b8779f0ae2a59c3a0b5bf3c91b5
Justyn Hunter <cjhunter@me.com>
2024-12-27 13:45:08 -0600

    adds footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,3 +1,9 @@
+import styles from "./style.module.css";
+
 export function Footer() {
-    return <p>not implemented</p>;
+    return (
+        <footer className={styles.container}>
+            copyright {new Date().getFullYear()}, all rights reserved
+        </footer>
+    );
 }
