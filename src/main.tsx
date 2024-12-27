86498e2c267e7bb2012a75c14afa1ad498caa70d
Justyn Hunter <cjhunter@me.com>
2024-12-27 10:17:45 -0600

    do routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,24 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Footer } from "@components/footer";
+import { Header } from "@components/header";
+import { About } from "@pages/about";
+import { Contact } from "@pages/contact";
+import { Home } from "@pages/home";
+import { Project } from "@pages/project";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />} />
-        </Routes>
-        <App />
+        <Header />
+        <main>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/:slug" element={<Project />} />
+            </Routes>
+        </main>
+        <Footer />
     </BrowserRouter>,
-)
+);
