0d6a2a068917cbcd2f43a945f553dc203003ea02
Justyn Hunter <cjhunter@me.com>
2024-12-29 17:46:19 -0600

    stub out project page

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,3 +1,30 @@
+import { getProject } from "@lib/strapiUtil";
+import { ProjectContent } from "@lib/types";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
 export function Project() {
-    return <p>project not implemented</p>;
+    const { slug } = useParams();
+
+    const [content, setContent] = useState<ProjectContent | null>(null);
+
+    useEffect(() => {
+        const fetch = async () => {
+            const data = await getProject(slug ?? "");
+            console.log("data", data);
+            setContent(data);
+        };
+
+        fetch();
+    }, []);
+
+    return content && (
+        <div>
+            <h1>{content.title}</h1>
+            {content.photographs.map((photo) => {
+                // TODO: setup CDN
+                return <img src={"http://localhost:1337" + photo.url} />;
+            })}
+        </div>
+    );
 }
