0dcc5bcca4835c73863a1c588b965b2c2ce843cc
Justyn Hunter <cjhunter@me.com>
2025-01-05 21:16:37 -0600

    contact form works

--- app/routes/contact/route.tsx
+++ app/routes/contact/route.tsx
@@ -1,10 +1,30 @@
-import { MetaFunction, useLoaderData } from "@remix-run/react"
+import { Form, MetaFunction, useActionData, useLoaderData } from "@remix-run/react"
 import { getContact } from "~/lib/strapiUtil";
 import styles from "./style.module.css";
 import { BlocksRenderer } from "@strapi/blocks-react-renderer";
+import clsx from "clsx";
+import { Resend } from "resend";
+import { ActionFunctionArgs } from "@remix-run/node";
 
 export const loader = async () => {
     return await getContact();
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+    const body = await request.formData();
+    const name = body.get("name");
+    const message = body.get("message");
+
+    const resend = new Resend(process.env.RESEND_KEY ?? "no key found");
+    const response = await resend.emails.send({
+        from: "do_not_reply@justynhunter.com",
+        to: "cjhunter@me.com",
+        subject: "Website Message!",
+        html: `<p>${name ?? "no name"}</p><p>${message ?? "no message"}</p>`
+    });
+
+    console.log("response", response);
+    return response;
 }
 
 export const meta: MetaFunction = () => {
@@ -15,20 +35,30 @@ export const meta: MetaFunction = () => {
 }
 
 export default function Contact() {
-    const content = useLoaderData<typeof loader>();
+    const data = useLoaderData<typeof loader>();
+    const formData = useActionData<typeof action>();
 
-    return (
-        <div className={styles.container}>
-            {content.content &&
-                <BlocksRenderer content={content.content} />
-            }
-            <div className={styles.contactForm}>
-            </div>
-            {content.successContent &&
-                <div className={styles.success}>
-                    <BlocksRenderer content={content.successContent} />
+    return (<div className={styles.container}>
+        {!formData && data.content &&
+            <BlocksRenderer content={data.content} />
+        }
+        {!formData &&
+            <Form className={styles.form} method="post" action="?index">
+                <div className={styles.formItem}>
+                    <label htmlFor="name">name</label>
+                    <input className={styles.formInput} name="name" />
                 </div>
-            }
-        </div>
-    );
+                <div className={styles.formItem}>
+                    <label htmlFor="message">message</label>
+                    <textarea className={clsx(styles.formInput, styles.textarea)} name="message" />
+                </div>
+                <button className={styles.formButton} type="submit">send</button>
+                {/*<Turnstile siteKey={import.meta.env.VITE_TURNSTILE_SITE_KEY} />*/}
+            </Form>}
+        {formData &&
+            <div>
+                <BlocksRenderer content={data.successContent} />
+            </div>
+        }
+    </div>);
 }
